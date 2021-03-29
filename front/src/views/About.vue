94a8e593e544e838033976109966627fdaf317e1
Arthur-bat <arthur.guiraud@esme.fr>
2021-03-29 16:10:35 +0200

    Page livraison

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="about">
+
     <h1>This is an about page</h1>
-    Tsume SA
-Tsume SA was created in Luxemburg in 2010. Along the years, the company managed to rise on the collector statue's market. Thanks to the backup of renown Japanese licenses, Tsume gave birth to a panel of figures aimed for the demanding collectors. After several years of hard work, Tsume has gained the trust of some of the main corporations in the world of manga and anime, like Shueisha, Toei Animation, TV Tokyo or Kodansha.
+      
+    ZFS - MDC S.A.R.L was created in Paris in 2019. Along the years, the company managed to rise on the collector statue's market. Thanks to the backup of renown Japanese licenses, Tsume gave birth to a panel of figures aimed for the demanding collectors. After several years of hard work, Tsume has gained the trust of some of the main corporations in the world of manga and anime, like Shueisha, Toei Animation, TV Tokyo or Kodansha.
 
-The Claw!
-The Claw! We attach our utmost importance to the quality of our products, we hope to reach excellence for each of our project, which is why everything should be well thought out. We really wish to bring our personal touch, while respecting the work of the authors, kind of like a claw mark on these luxury statues.
+    The Claw!
+    The Claw! We attach our utmost importance to the quality of our products, we hope to reach excellence for each of our project, which is why everything should be well thought out. We really wish to bring our personal touch, while respecting the work of the authors, kind of like a claw mark on these luxury statues.
 
-Cyril Marchiol
-Cyril Marchiol, founder of the company and his team have come to work on several licenses like Naruto Shippuden, Fairytail, Bleach, Saint Seiya “Les Chevaliers du Zodiaque”, One Piece, Eyeshield 21, Soul Eater, Street Fighter IV Ultra, Asura’s Wrath, Black Butler, Dreamland and many more to come.
+    Arthur and Emeric
+    Arthur and Emeric, founders of the company and their team have come to work on several licenses like Fairy Tail, Bleach, Dragon Ball Z, Berserk, Shingeki no Kyojin, Van Hellsing, Naruto, etc.
 
 
   </div>
 </template>
+
+<style scoped>
+
+.about{
+  color:white;
+  margin-bottom: 30px;
+}
+
+</style>
