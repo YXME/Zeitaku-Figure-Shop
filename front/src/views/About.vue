5c83726f5cb679273ea5c2d86837c925358279f2
Arthur-bat <arthur.guiraud@esme.fr>
2021-03-30 10:44:01 +0200

    Page about

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="about">
 
-    <h1>This is an about page</h1>
+    <h1><center>A propos</center></h1>
       
-    ZFS - MDC S.A.R.L was created in Paris in 2019. Along the years, the company managed to rise on the collector statue's market. Thanks to the backup of renown Japanese licenses, Tsume gave birth to a panel of figures aimed for the demanding collectors. After several years of hard work, Tsume has gained the trust of some of the main corporations in the world of manga and anime, like Shueisha, Toei Animation, TV Tokyo or Kodansha.
-
-    The Claw!
-    The Claw! We attach our utmost importance to the quality of our products, we hope to reach excellence for each of our project, which is why everything should be well thought out. We really wish to bring our personal touch, while respecting the work of the authors, kind of like a claw mark on these luxury statues.
-
-    Arthur and Emeric
-    Arthur and Emeric, founders of the company and their team have come to work on several licenses like Fairy Tail, Bleach, Dragon Ball Z, Berserk, Shingeki no Kyojin, Van Hellsing, Naruto, etc.
+    ZFS - MDC S.A.R.L a été créée à Paris en 2015. Au fil des années, l'entreprise réussit à se hisser sur le marché des statues de collection. <br>
+    Grâce à la sauvegarde de licences japonaises de renom, ZFS a donné naissance à un panel de personnages destinés aux collectionneurs exigeants.
+    Après plusieurs années de travail acharné, ZFS a gagné la confiance de certaines des principales sociétés du monde du manga et de l'anime, comme Shueisha, Toei Animation, TV Tokyo ou Kodansha.<br>
+    <br>
+    Nous attachons la plus grande importance à la qualité de nos produits, nous espérons atteindre l'excellence pour chacun de nos projets, c'est pourquoi tout doit être bien pensé. <br>
+    <br>
+    Nous souhaitons vraiment apporter notre touche personnelle, tout en respectant le travail des auteurs, un peu comme une marque de griffe sur ces statues de luxe.
+    
+    <br>
+    <br>
+    Arthur et Emeric, fondateurs de la société et leur équipe sont venus travailler sur plusieurs licences comme Fairy Tail, Bleach, Dragon Ball Z, Berserk, Shingeki no Kyojin, Van Hellsing, Naruto, etc.
 
 
   </div>
@@ -20,6 +24,7 @@
 .about{
   color:white;
   margin-bottom: 30px;
+  text-align:justify;
 }
 
 </style>
