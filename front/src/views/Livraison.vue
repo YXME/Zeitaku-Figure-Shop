55c034d15d62f97d76b5b6e4a1cef5c193685998
Arthur-bat <arthur.guiraud@esme.fr>
2021-03-30 10:37:15 +0200

    Livraison tableau

--- front/src/views/Livraison.vue
+++ front/src/views/Livraison.vue
@@ -2,56 +2,284 @@
 
     <div class ="tab">
         
-        La liste ci-dessous présente les différentes méthodes de livraison des colis sur chaque continent. Pour rechercher votre pays, merci de vous référer au contient dans lequel est votre pays.
+        <h1> Tarifs applicables pour le transport de nos statues</h1>
 
-        *Nous essayons de maintenir cette liste à jour régulièrement.
+        <p>
+            La liste ci-dessous présente les différents transporteurs prenant en charge la livraison des colis sur chaque continent. Pour rechercher le tarif applicable dans votre pays de résidence, merci de vous référer au tarif en vigueur sur le contient relatif au pays souhaité.<br>
+            <br>
+            Tous nos produits, possèdent un carton, un surcarton, un sur emballage pour éviter tout type de casse lors du transport.<br>
 
-        Si vous avez des questions, n'hésitez pas à contacter notre support.
+            les tarifs que nous appliquons, prennent en compte le transport des marchandises en avion, en camion ou en bateau. <br>
+            <br>
+            Si vous avez des questions ou des réclamations, n'hésitez pas à contacter notre support technique.<br>
+        </p>
 
         <br>
         <br>
 
             <table border="1" width="100%">
                 <tr>
-                    <td width="25%" style="font-weight:bold">Contient</td>
-                    <td width="25%" style="font-weight:bold">DHL</td>
-                    <td width="25%" style="font-weight:bold">EMS</td>
+                    <td width="20%" style="font-weight:bold">Transporteur <br> / <br> Contient</td>
+                    <td width="20%" style="font-weight:bold">DHL</td>
+                    <td width="20%" style="font-weight:bold">EMS</td>
+                    <td width="20%" style="font-weight:bold">Chronopost</td>
+                    <td width="20%" style="font-weight:bold">FEDEX</td>
 
                 </tr>
                 <tr>
-                    <td width="25%">Amérique</td>
-                    <td width="25%">●</td>
-                    <td width="25%"></td>
+                    <td width="20%">Amérique</td>
+                    <td width="20%">●</td>
+                    <td width="20%"></td>
+                    <td width="20%"></td>
+                    <td width="20%">●</td>
                 </tr>
                 <tr>
-                    <td width="25%">Océanie</td>
-                    <td width="25%"> </td>
-                    <td width="25%">●</td>
+                    <td width="20%">Océanie</td>
+                    <td width="20%"> </td>
+                    <td width="20%">●</td>
+                    <td width="20%"></td>
+                    <td width="20%"></td>
                 </tr>
                 <tr>
-                    <td width="25%">Europe</td>
-                    <td width="25%">●</td>
-                    <td width="25%">●</td>
+                    <td width="20%">Europe</td>
+                    <td width="20%">●</td>
+                    <td width="20%">●</td>
+                    <td width="20%">●</td>
+                    <td width="20%">●</td>
                 </tr>
                 <tr>
-                    <td width="25%">Asie</td>
-                    <td width="25%">●</td>
-                    <td width="25%"></td>
+                    <td width="20%">Asie</td>
+                    <td width="20%">●</td>
+                    <td width="20%"></td>
+                    <td width="20%"></td>
+                    <td width="20%">●</td>
                 </tr>
-                   
-                 
+            </table>
+            <br>
+            <br>
+
+            <table border="1" width="100%">
+                <tr>
+                    <td width="20%" style="font-weight:bold">Continent<br> /<br> Poids du produit</td>
+                    <td width="20%" style="font-weight:bold">Amérique</td>
+                    <td width="20%" style="font-weight:bold">Europe</td>
+                    <td width="20%" style="font-weight:bold">Asie</td>
+                    <td width="20%" style="font-weight:bold">Océanie</td>
+
+                </tr>
+                <tr>
+                    <td width="20%">de 15 à 25kg</td> <!--ligne du tableau-->
+
+                    <td width="20%"> <!-- fonction qui permet de cacher le montant de la statue en Yen, au passage de la souris celle-ci apparait-->
+                        <a href="#">150€
+                            <span>19416¥</span>
+                        </a>
+                    </td>
+
+                    <td width="20%">
+                        <a href="#">50€
+                            <span>6472¥</span>
+                        </a>
+                    </td>
+
+                    <td width="20%">
+                        <a href="#">280€
+                            <span>36243¥</span>
+                        </a>
+                    </td>
+
+                    <td width="20%">
+                        <a href="#">350€
+                            <span>45304¥</span>
+                        </a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td width="20%">de 25 à 35kg</td>
+
+                    <td width="20%">
+                        <a href="#">290€
+                            <span>37538¥</span>
+                        </a>
+                    </td>
+
+                    <td width="20%">
+                        <a href="#">150€
+                            <span>19416¥</span>
+                        </a>
+                    </td>
+
+                    <td width="20%">
+                        <a href="#">360€
+                            <span>46598¥</span>
+                        </a>
+                    </td>
+                    <td width="20%">
+                        <a href="#">490€
+                            <span>63426¥</span>
+                        </a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td width="20%">de 35 à 45kg</td>
+
+                    <td width="20%">
+                        <a href="#">380€
+                            <span>49187¥</span>
+                        </a>
+                    </td>
+
+                    <td width="20%">
+                        <a href="#">250€
+                            <span>32360¥</span>
+                        </a>
+                    </td>
+
+                    <td width="20%">
+                        <a href="#">420€
+                            <span>54365¥</span>
+                        </a>
+                    </td>
+
+                    <td width="20%">
+                        <a href="#">740€
+                            <span>95785¥</span>
+                        </a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td width="20%">de 45 à 55kg</td>
+
+                    <td width="20%">
+                        <a href="#">510€
+                            <span>66014¥</span>
+                        </a>
+                    </td>
+
+                    <td width="20%">
+                        <a href="#">300€
+                            <span>38832¥</span>
+                        </a>
+                    </td>
+
+                    <td width="20%">
+                        <a href="#">750€
+                            <span>97080¥</span>
+                        </a>
+                    </td>
+
+                    <td width="20%">
+                        <a href="#">820€
+                            <span>106141¥</span>
+                        </a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td width="20%">de 55 à 65kg</td>
+
+                    <td width="20%">
+                        <a href="#">640€
+                            <span>82841¥</span>
+                        </a>
+                    </td>
+
+                    <td width="20%">
+                        <a href="#">450€
+                            <span>58248¥</span>
+                        </a>
+                    </td>
+
+                    <td width="20%">
+                        <a href="#">850€
+                            <span>110024¥</span>
+                        </a>
+                    </td>
+
+                    <td width="20%">
+                        <a href="#">900€
+                            <span>116496¥</span>
+                        </a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td width="20%">de 65 à 75kg</td>
+
+                    <td width="20%">
+                        <a href="#">700€
+                            <span>90608¥</span>
+                        </a>
+                    </td>
+
+                    <td width="20%">
+                        <a href="#">500€
+                            <span>64720¥</span>
+                        </a>
+                    </td>
+
+                    <td width="20%">
+                        <a href="#">1000€
+                            <span>129440¥</span>
+                        </a>
+                    </td>
+
+                    <td width="20%">
+                        <a href="#">1500€
+                            <span>194160¥</span>
+                        </a>
+                    </td>
+                </tr>
+                    
             </table>
     </div>	
 
 </template>
 
 <style scoped>
+p{
+    text-align:left;
+}
 
 .tab{
     color:white;
     margin-bottom: 30px;
+    margin-left:20px;
+    margin-right:20px;
 }
 
+td{
+   height:30px;
+}
 
-Rajouter, un tableau des prix par rapport aux poids des statues, faire du bullshit, mettre des marges au tableau, et un peu plus de texte.
+a{
+     color:#ccc;
+     text-shadow:0 -1px 0 black;                
+}
+a:hover,a:focus{
+     background:rgba(0,0,0,.4);
+     box-shadow:0 1px 0 rgba(255,255,255,.4);
+}
+
+a span{
+    position:absolute;   
+    color: #EAEE59;
+    background:rgba(0,0,0,.9);
+    padding:15px;
+    border-radius:3px;
+    box-shadow:0 0 2px rgba(0,0,0,.5);     
+    margin-top:23px;
+    margin-left:-35px;   
+    transform:scale(0) rotate(-12deg); 
+    transition:all .25s;
+    opacity:0;    
+}
+a:hover span, a:focus span{
+     transform:scale(1) rotate(0);   
+     opacity:1;     
+}
+
 </style>
